9df79b52789f10bf2050da80c230717a841da0f6
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace NewItemRow {
+
+    public partial class Window1 : Window {
+        BindingList<TestData> list;
+
+        public Window1() {
+            InitializeComponent();
+
+            #region Fill the GridControl with data
+            list = new BindingList<TestData>();
+            for (int i = 0; i < 5; i++) {
+                list.Add(new TestData() { Text1 = Guid.NewGuid().ToString(), 
+                    Text2 = "text2 " + i, Text3 = "text3 " + i });
+            }
+            grid.DataSource = list;
+            #endregion
+        }
+    }
+
+    #region Test data
+    public class TestData {
+        public string Text1 { get; set; }
+        public string Text2 { get; set; }
+        public string Text3 { get; set; }
+    }
+    #endregion
+
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to add a new row using the New Item Row
+
+
+<p>The following example demonstrates how to enable the <strong>New Item Row</strong> feature.</p>
+
+
+<h3>Description</h3>
+
+<p>The following example demonstrates how to enable the <strong>New Item Row</strong> feature of the <strong>TableView</strong>. To do this, it&#39;s necessary to set the <strong>NewItemRowPosition</strong> property to a value different from <strong>None</strong>.</p>
+
+<br/>
+
+
